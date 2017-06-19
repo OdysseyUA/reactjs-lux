94e6b7fd7fd5754cac0abd10c479699928c6fe35
Adnrey Zhuvago <azhuvago@luxoft.com>
2017-06-19 21:01:04 +0300

    Router: Middleware (not finished correct routing)

--- js/user-details.js
+++ js/user-details.js
@@ -12,6 +12,23 @@ let user = {
 	skills: ["html5", "css3", "react"]
 };
 
+let detailsRecords = [
+	{
+		id: 1,
+		name: "John Doe",
+		about: "Nice guy",
+		hobbies: "Likes drinking wine",
+		skills: ["html", "javascript", "redux"]
+	},
+	{
+		id: 2,
+		name: "Mary Moe",
+		about: "Cute girl",
+		hobbies: "Likes playing xbox whole days long",
+		skills: ["Fortran", "Lua", "R#"]
+	}
+];
+
 export default class UserDetails extends React.Component {
 
 	constructor() {
@@ -23,7 +40,20 @@ export default class UserDetails extends React.Component {
 
 
 	componentWillMount() {
-		this.setState({user: user})
+		let user;
+		// this.setState({user: user});
+		// console.log(this.props);
+		// console.log(this.props.params);
+		// console.log(':: ' + this.props.params.id);
+		// console.log(':: ' + detailsRecords[this.props.params.id-1].name);
+		if(this.props.params.id) {
+			user = detailsRecords.filter((user) => user.id == this.props.params.id);
+			this.setState({user: user[0]});
+		}
+		else {
+			user = detailsRecords[1];
+			this.setState({user: user});
+		}
 	}
 
 
@@ -36,15 +66,17 @@ export default class UserDetails extends React.Component {
 						<div className="well profile">
 							<div className="col-sm-12">
 								<div className="col-xs-12 col-sm-8">
-									<h2>Nicole Pearson</h2>
-									<p><strong>About: </strong> {user.prof} </p>
-									<p><strong>Hobbies: </strong> {user.hobbies} </p>
+									<h2>{this.state.user.name}</h2>
+									<p><strong>About: </strong> {this.state.user.prof} </p>
+									<p><strong>Hobbies: </strong> {this.state.user.hobbies} </p>
 									<p><strong>Skills: </strong>
 										{/*<span className="tags">html5</span>*/}
 										{/*<span className="tags">css3</span>*/}
 										{/*<span className="tags">jquery</span>*/}
 										{/*<span className="tags">bootstrap3</span>*/}
-										{user.skills.map((skill, index) => {return <span key={index} className="tags" style={{margin: 5}}>{skill}</span>})}
+										{user.skills.map((skill, index) => {
+											return <span key={index} className="tags" style={{margin: 5}}>{skill}</span>
+										})}
 									</p>
 								</div>
 								<div className="col-xs-12 col-sm-4 text-center">
@@ -61,6 +93,4 @@ export default class UserDetails extends React.Component {
 			</div>
 		)
 	}
-
-
 }
